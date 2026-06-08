142d227d7a143213fbdba471c094e65668f71fd2
akhdanaqilla-droid <akhdan.aqilla@mhs.unsoed.ac.id>
2026-06-08 15:22:27 +0700

    Update 01_analisis_teks.R

--- src_R/01_analisis_teks.R
+++ src_R/01_analisis_teks.R
@@ -1,6 +1,6 @@
 teks <-"Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
 
-kata <- 0
+kata <- 1
 kalimat <- 0
 
 for(i in 1:nchar(teks)){
@@ -21,7 +21,7 @@ cat("Teks tersebut memuat", kalimat,
     "kalimat dan", kata, "kata.")
 
 teks <-"Belajar pemrograman itu menyenangkan."
-kata <- 0
+kata <- 1
 kalimat <- 0
 
 for(i in 1:nchar(teks)){
@@ -42,7 +42,7 @@ cat("Teks tersebut memuat", kalimat,
     "kalimat dan", kata, "kata.")
 
 teks <-"Alpro."
-kata <- 0
+kata <- 1
 kalimat <- 0
 
 for(i in 1:nchar(teks)){
