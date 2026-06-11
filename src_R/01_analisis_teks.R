ec77c6a50b51a66d02c715874ac354b453cf3714
akhdanaqilla-droid <akhdan.aqilla@mhs.unsoed.ac.id>
2026-06-11 16:14:31 +0700

    Update 01_analisis_teks.R

--- src_R/01_analisis_teks.R
+++ src_R/01_analisis_teks.R
@@ -1,64 +1,19 @@
-teks <-"Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
-
-kata <- 1
-kalimat <- 0
-
-for(i in 1:nchar(teks)){
-  
-  huruf <- substr(teks, i, i)
-  
-  if(huruf == " "){
-    kata <- kata + 1
+hitung_teks <- function(input_teks) {
+  if (nchar(input_teks) == 0) {
+    kata <- 0
+    kalimat <- 0
+  } else {
+    kata <- 1
+    kalimat <- 0
+    for(i in 1:nchar(input_teks)){
+      huruf <- substr(input_teks, i, i)
+      if(huruf == " ") kata <- kata + 1
+      if(huruf == ".") kalimat <- kalimat + 1
+    }
   }
-  
-  if(huruf == "."){
-    kalimat <- kalimat + 1
-  }
-  
+  cat("Teks tersebut memuat", kalimat, "kalimat dan", kata, "kata.\n")
 }
 
-cat("Teks tersebut memuat", kalimat,
-    "kalimat dan", kata, "kata.")
-
-teks <-"Belajar pemrograman itu menyenangkan."
-kata <- 1
-kalimat <- 0
-
-for(i in 1:nchar(teks)){
-  
-  huruf <- substr(teks, i, i)
-  
-  if(huruf == " "){
-    kata <- kata + 1
-  }
-  
-  if(huruf == "."){
-    kalimat <- kalimat + 1
-  }
-  
-}
-
-cat("Teks tersebut memuat", kalimat,
-    "kalimat dan", kata, "kata.")
-
-teks <-"Alpro."
-kata <- 1
-kalimat <- 0
-
-for(i in 1:nchar(teks)){
-  
-  huruf <- substr(teks, i, i)
-  
-  if(huruf == " "){
-    kata <- kata + 1
-  }
-  
-  if(huruf == "."){
-    kalimat <- kalimat + 1
-  }
-  
-}
-
-cat("Teks tersebut memuat", kalimat,
-    "kalimat dan", kata, "kata.")
-
+hitung_teks("Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif.")
+hitung_teks("Belajar pemrograman itu menyenangkan.")
+hitung_teks("")
